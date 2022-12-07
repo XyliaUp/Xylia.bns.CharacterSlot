9fedf8842f3b708556841b30d941163f076866f1
Xylia <xueyi@bnszs.com>
2022-12-07 18:07:58 +0800

    初始提交

---
+++ CharacterSlot/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CharacterSlot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(154, 28);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(102, 24);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("宋体", 15F);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(12, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "角色顺序";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("宋体", 15F);
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(12, 83);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(119, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "等待获取...";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.Color.Black;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(311, 141);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.DoubleBuffered = true;
+			this.ForeColor = System.Drawing.Color.DimGray;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximumSize = new System.Drawing.Size(1840700304, 611);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "灵灵角色获取";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		//private Xylia.Preview.Project.Controls.PanelEx.PanelContent panelContent1;
+		//private Project.Core.Item.Cell.Basic.FrameIconCell frameIconCell1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ CharacterSlot/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace CharacterSlot
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			//标题文本
+			//this.Text = $"{ this.Text } ({ System.Reflection.Assembly.GetExecutingAssembly().ImageRuntimeVersion })";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			for (int i = 1; i <= 20; i++) this.comboBox1.Items.Add(i.ToString());
+
+			this.comboBox1.SelectedIndex = 0;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var Configuration = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\BnS\TENCENT\ClientConfiguration.xml";
+			if (!File.Exists(Configuration))
+			{
+				label2.Text = "请先登录一次游戏";
+				return;
+			}
+
+
+			var XmlDoc = new XmlDocument();
+			XmlDoc.Load(Configuration);
+
+			var SlotOrder = XmlDoc.SelectSingleNode($@"config/option[@name='character-slot-order-{this.comboBox1.Text}']");
+			if(SlotOrder is null)
+			{
+				label2.Text = "参数无效";
+				return;
+			}
+
+
+			if(!int.TryParse(SlotOrder.Attributes["value"]?.Value,out int result) || result == 0)
+			{
+				label2.Text = "无效角色编号";
+				return;
+			}
+
+
+			if (true && result > 50000)
+			{
+				label2.Text = "非体验服角色，请先登录体验服后再试";
+				return;
+			}
+
+			label2.Text =$"当前角色编号: {result}";
+		}
+	}
+}
